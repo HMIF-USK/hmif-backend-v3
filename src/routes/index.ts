020c1ec196f076c7317bb80cb3dbe2684d5fb4fc
unknown <rahiqulmunadhil@gmail.com>
2026-05-07 18:23:18 +0700

    add activities module

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import authRouter from "@/module/auth/AuthRouter";
+import activityRouter from "@/module/activities/ActivityRouter";
 
 const router = Router();
 
 router.use("/auth", authRouter);
+router.use("/activities", activityRouter); 
 
 export default router;
