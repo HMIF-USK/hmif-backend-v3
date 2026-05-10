40a36d9b639d26ebea3fbd7dbfd70edd936dc484
Muhammad Fariz Alfisaputra <mfarizalfisaputra@gmail.com>
2026-05-10 21:05:55 +0700

    feat: add achievements module with GET endpoints

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import authRouter from "@/module/auth/AuthRouter";
+import achievementsRouter from "@/module/achievements/AchievementsRouter";
 
 const router = Router();
 
 router.use("/auth", authRouter);
+router.use("/achievements", achievementsRouter);
 
 export default router;
