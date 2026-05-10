1b7fce970e85ed1f733d2cff975f2b29baeb753e
test <24nurilala@mhs.usk.ac.id>
2026-05-10 11:50:49 +0700

    feat: add achievement(mba) module (POST, PUT, DELETE)

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import authRouter from "@/module/auth/AuthRouter";
+import achievementRouter from "@/module/achievement/Achievementrouter";
 
 const router = Router();
 
 router.use("/auth", authRouter);
+router.use("/achievements", achievementRouter);
 
 export default router;
