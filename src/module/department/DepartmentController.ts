7abf52559c1587398272f7cb7d12901e55891ae8
Rahmatul Uliya <uliya24mhs.usk.ac.id>
2026-06-09 22:24:08 +0700

    fix: perbaiki error DepartmentController

--- src/module/department/DepartmentController.ts
+++ src/module/department/DepartmentController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import DepartmentService from "./DepartmentService.ts";
+import DepartmentService from "./DepartmentService";
 
 class DepartmentController {
 
